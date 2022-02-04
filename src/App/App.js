b9cfffc861fd247a01c9e51e4d1c37c78f9fa770
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-02-04 17:43:41 +0200

    add-structures-react
    добавляем структуру React и стартовые файлы

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import { AppGlobal } from "./App.styled";
+
+function App() {
+  return (
+    <AppGlobal>
+      <header>
+        <p>TASK2 goit-react-hw-02-feedback</p>
+      </header>
+    </AppGlobal>
+  );
+}
+
+export default App;
